67e2ea151f971f8608bd6ba078f6f13d6d02cb4c
PhanPhuc269 <164202651+PhanPhuc269@users.noreply.github.com>
2024-12-10 21:36:40 +0700

    Bổ sung frontend

---
+++ frontend/src/config/routes.js
@@ -0,0 +1,13 @@
+const routes = {
+    home: '/',
+    following: '/following',
+    profile: '/@:nickname',
+    watch: '/watch',
+    search: '/search',
+    myCourse: '/my-course',
+    login: '/login',
+    dashboard: '/dashboard',
+    course: '/course',
+};
+
+export default routes;
