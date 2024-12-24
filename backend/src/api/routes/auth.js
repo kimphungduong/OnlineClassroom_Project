88d45638f7becbeb270e902820ec2d9d3aa1d185
kimphungduong <142092690+kimphungduong@users.noreply.github.com>
2024-12-24 23:46:46 +0700

    database

--- backend/src/api/routes/auth.js
+++ backend/src/api/routes/auth.js
@@ -3,7 +3,7 @@ const router =express.Router();
 
 const authController= require('../controllers/AuthController');
 
-router.post('/auth/google',authController.google);
+// router.post('/auth/google',authController.google);
 router.post('/login',authController.login);
 router.post('/refresh-token',authController.requestRefreshToken);
 router.post('/logout',authController.logout);
